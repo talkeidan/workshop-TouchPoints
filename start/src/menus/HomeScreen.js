768bdfcd961c0de5e68f9cae01dcca82917b33fe
Tal <talkeidan@gmail.com>
2020-08-25 20:01:16 +0300

    added music for home screen

--- start/src/menus/HomeScreen.js
+++ start/src/menus/HomeScreen.js
@@ -1,15 +1,38 @@
-import React from "react";
-import { Text, StyleSheet, View, TouchableOpacity, ImageBackground} from "react-native";
+import React, {useState} from "react";
+import { StyleSheet, View, TouchableOpacity, ImageBackground} from "react-native";
 import ButtonsMenu from "../components/ButtonsMenu"
 import { Entypo } from "@expo/vector-icons";
 import { Col, Row, Grid } from "react-native-easy-grid";
 import normalize from "react-native-normalize";
+import { Video, Audio } from 'expo-av';
 
-const HomeScreen = ({navigation}) => {
+
+export default HomeScreen = ({navigation}) =>
+{
+  const [isPlay, setIsPlay] = useState(true);
+    
+    React.useEffect(
+      () => navigation.addListener('focus', () => setIsPlay(true)),
+      []
+    );
+  
+    React.useEffect(
+      () => navigation.addListener('blur', () => setIsPlay(false)),
+      []
+    );
   return <ImageBackground style={styles.bgimage} source={require("../../assets/stars.png")} resizeMode="cover">
+    <Video
+    source={require("../../assets/playground.mp3")}
+    shouldPlay={isPlay}
+    isLooping={true}
+    volume={0.1}
+    useNativeControls ={false}
+  />
   <Grid style={{flex: 1}}>
     <Row style= {styles.list1}>
-      <ButtonsMenu title = "מבוא" navigate = {() => navigation.navigate('Intro')}/>
+      <ButtonsMenu title = "מבוא" navigate = {() => {
+        navigation.navigate('Intro')
+        }}/>
       <ButtonsMenu title = "לימוד ספרות" navigate = {() => navigation.navigate('LearningDigitsMenu')}/>
       <ButtonsMenu title = "זיהוי ספרות" navigate = {() => navigation.navigate('IdentifyDigits')}/>
     </Row>
@@ -24,7 +47,7 @@ const HomeScreen = ({navigation}) => {
   </ImageBackground>
 };
 
-const styles = StyleSheet.create({
+styles = StyleSheet.create({
   text: {
     fontSize: 30,
     textAlign: 'center'
@@ -53,40 +76,3 @@ const styles = StyleSheet.create({
     flex: 1,
   }
 });
-
-export default HomeScreen;
-
-/*import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import joey from './assets/joey.png';
-import { Col, Row, Grid } from "react-native-easy-grid";
-
-export default function App() {
-  return (
-    <Grid>
-      <Row style={{ justifyContent: "space-evenly", marginTop: "5%", alignItems: "flex-end"}}>
-      <Image source={joey} style={{width:"20%", height:"60%", borderRadius:200, borderWidth:10, borderColor:'#994d00'}}/>
-      <Image source={joey} style={{width:"20%", height:"60%", borderRadius:200, borderWidth:10, borderColor:'#994d00'}}/>
-      <Image source={joey} style={{width:"20%", height:"60%", borderRadius:200, borderWidth:10, borderColor:'#994d00'}}/>
-      </Row>
-      <Row style={{ justifyContent: "space-evenly", marginTop: "5%", alignItems: "flex-start"}}>
-      <Image source={joey} style={{width:"10%", height:"30%", borderRadius:100, borderWidth:5, borderColor:'#994d00', position: "absolute", left: "5%", bottom: "16%"}}/>
-      <Image source={joey} style={{width:"20%", height:"60%", borderRadius:200, borderWidth:10, borderColor:'#994d00'}}/>
-      <Image source={joey} style={{width:"20%", height:"60%", borderRadius:200, borderWidth:10, borderColor:'#994d00'}}/>
-      </Row>
-    </Grid>
-    // <View style={styles.container}>
-    //   <Text style={{color:'gray', fontSize: 40}}>hello Joey!</Text>
-    //   <Image source={joey} style={{width:305, height:400, borderRadius:200, borderWidth:10, borderColor:'#994d00'}}/>
-    //</View>
-  );
-}
-
-const styles = StyleSheet.create({//background style
-  container: {
-    flex: 1,
-    backgroundColor: '#99ffeb',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});*/
